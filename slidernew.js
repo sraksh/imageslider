68e75b79e2b2048e5c10dfce025cc6920f2c0475
Sumit <sumitkumarrakshit@gmail.com>
2016-09-03 10:41:56 +0530

    Final version

--- slidernew.js
+++ slidernew.js
@@ -23,19 +23,14 @@ function ajaxfn() {
                 if (kc == 37) slide(1);
                 else if (kc == 39) slide(-1);
             }
-
-            //t.innerHTML = arr[imgCount].title;
             window.addEventListener("keydown", titlefn);
             window.addEventListener("click", titlefn);
             titlefn();
 
             function titlefn() {
-                //console.log("Title index:" + imgCount);
                 console.log("title click: " + arr[imgCount].title)
-                t.innerHTML = arr[imgCount].title
-
+                t.innerHTML = arr[imgCount].title;
             }
-            //console.log(imgblock);
             imgFrame = 0;
             imgArray = document.querySelectorAll('#imgSlider img');
             var l = document.getElementById('left');
@@ -48,41 +43,46 @@ function ajaxfn() {
             });
 
             function slide(x) {
-                //console.log(imgFrame)
-                if (imgFrame === 0 && x == 1) {
-                    imgCount = 4;
-                    for (var i = 0; i < imgArray.length; i++) {
-                        var temp = imgArray[i];
-                        temp.style.transform = 'translate(' + (-3200) + 'px)';
-                        console.log('translate(' + (imgFrame) + 'xpx)')
-                    }
-                    imgFrame = -3200;
-                    console.log("breakpnt2: " + imgCount)
-
-                } else if (imgFrame === -3200 && x === -1) {
+                if (imgCount == 4 && x == -1) {
                     imgCount = 0;
-                    for (var i = 0; i < imgArray.length; i++) {
-                        var temp = imgArray[i];
-                        temp.style.transform = 'translate(0px)';
-                        console.log('translate(' + (imgFrame) + 'px)')
-                    }
-                    imgFrame = 0;
-                    console.log("breakpnt1: " + imgCount)
-
+                } else if (imgCount == 0 && x == 1) {
+                    imgCount = 4;
                 } else {
                     imgCount -= x;
-                    imgFrame += (x * 800);
-                    for (var i = 0; i < imgArray.length; i++) {
-                        var temp = imgArray[i];
-                        temp.style.transform = 'translate(' + (imgFrame) + 'px)';
-                        console.log('translate(' + (imgFrame) + 'opx)')
-                    }
-                    console.log("imgCount: " + imgCount)
                 }
+                imgFrame += (x * 72);
+                imgblock.style.transform = 'translateZ(-551px) rotateY(' + imgFrame + 'deg)';
+                console.log('rotateY(' + (imgFrame) + 'opx)')
+                console.log("imgCount: " + imgCount)
             }
+            one = document.getElementById('1');
+            two = document.getElementById('2');
+            three = document.getElementById('3');
+            four = document.getElementById('4');
+            five = document.getElementById('5');
+            one.addEventListener('click', function() {
+                imgblock.style.transform = 'translateZ(-551px) rotateY(' + (0) + 'deg)';
+                console.log('t.inner: ' + arr[3].title);
+                t.innerHTML = arr[0].title;
+            });
+            two.addEventListener('click', function() {
+                imgblock.style.transform = 'translateZ(-551px) rotateY(' + (-72) + 'deg)';
+                t.innerHTML = arr[1].title;
+            });
+            three.addEventListener('click', function() {
+                imgblock.style.transform = 'translateZ(-551px) rotateY(' + (-144) + 'deg)';
+                t.innerHTML = arr[2].title;
+            });
+            four.addEventListener('click', function() {
+                imgblock.style.transform = 'translateZ(-551px) rotateY(' + (-216) + 'deg)';
+                t.innerHTML = arr[3].title;
+            });
+            five.addEventListener('click', function() {
+                imgblock.style.transform = 'translateZ(-551px) rotateY(' + (-288) + 'deg)';
+                t.innerHTML = arr[4].title;
+            });
         }
     };
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
-
 }
